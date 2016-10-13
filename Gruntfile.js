afc4eeb56029d6fd5db7f5cca41350f7d143127c
Heyuqing <yuqing.he@xiaobaoonline.com>
2016-10-13 15:30:22 +0800

    hh

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        requirejs: {
+            compile: {
+                "options": {
+                    paths: {
+                        "module1": "app1/module1",
+                        "module2": "app2/module2",
+                    },
+                    optimize: "uglify",
+                    uglify: {
+                        // 是否混淆变量名
+                        mangle: false,
+                        compress: {
+                            //删除console.log
+                            drop_console: true,
+                            dead_code: true
+                        },
+                        //删除注释
+                        preserveComments: false,
+                    },
+                    baseUrl: "./demo",
+                    dir: 'built2',
+                    modules: [{
+                        name: 'module1',
+                        exclude:["module2"]
+                    }, {
+                        name: 'module2',
+                    }],
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    // grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('default', ['requirejs']);
+};
